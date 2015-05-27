bf1e9a2da506daa38ea2755616489c214d690d94
Anton <anton.blomster@gmail.com>
2015-05-27 10:47:45 +0200

    Implemented loading wheel (progressbar) while connecting to screen. Minor compability tweaks.

--- ScreenAndroid/src/main/java/se/mah/k3/klarappo/LoginFragment.java
+++ ScreenAndroid/src/main/java/se/mah/k3/klarappo/LoginFragment.java
@@ -12,6 +12,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -36,7 +37,7 @@ public class LoginFragment extends Fragment implements ValueEventListener
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View returnView = inflater.inflate(R.layout.fragment_login, container, false);
+        final View returnView = inflater.inflate(R.layout.fragment_login, container, false);
         View v = returnView.findViewById(R.id.btnLogon);
 
         //Imports Exo Bold & Exo Extra Light fonts
@@ -56,17 +57,40 @@ public class LoginFragment extends Fragment implements ValueEventListener
         Button btn = (Button)returnView.findViewById(R.id.btnLogon);
         btn.setTypeface(ExoB);
 
+        final ProgressBar prog = (ProgressBar)returnView.findViewById(R.id.progressBar);
+        prog.setVisibility(View.INVISIBLE);
+
 
         v.setOnClickListener(new View.OnClickListener() {
             //Click on loginButton
             @Override
             public void onClick(View v) {
-                //In firebase you read a value by adding a listener, then it will trigger once connected and on all changes.
-                //There is no readvalue as one could expect only listeners.
-                //Get the ScreenNbr child
-                Firebase  fireBaseEntryForScreenNbr = Constants.getFirebaseRef().child("ScreenNbr");
-                //Ok listen the changes will sho up in the method onDataChange
-                fireBaseEntryForScreenNbr.addValueEventListener(LoginFragment.this);
+
+                try {
+                    EditText name = (EditText) getActivity().findViewById(R.id.name);
+                    Constants.userName = name.getText().toString();
+                    //Are we on the right screen
+                    Log.i("name",Constants.userName);
+                    if (name.getText().toString().equals("")){
+                        Toast.makeText(getActivity(),getResources().getString(R.string.empty_name),Toast.LENGTH_LONG).show(); //Toast message if player hasn't entered name
+                    }else{
+                        prog.setVisibility(View.VISIBLE);
+
+                        //In firebase you read a value by adding a listener, then it will trigger once connected and on all changes.
+                        //There is no readvalue as one could expect only listeners.
+                        //Get the ScreenNbr child
+                        Firebase  fireBaseEntryForScreenNbr = Constants.getFirebaseRef().child("ScreenNbr");
+                        fireBaseEntryForScreenNbr.addValueEventListener(LoginFragment.this); //Ok listen the changes will sho up in the method onDataChange
+                        //String screenNumber = String.valueOf(Constants.screenNbr);
+                        //if (screenNbrFromFirebase.equals(screenNumber)){
+
+                        // }else{
+                        //   Toast.makeText(getActivity(),getResources().getString(R.string.toast_incorrect_screen),Toast.LENGTH_LONG).show();
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    Log.i("LoginFragment","onDataChanged failed");
+                }
             }
         });
         return returnView;
@@ -79,26 +103,14 @@ public class LoginFragment extends Fragment implements ValueEventListener
                 long val = (long) snapshot.getValue();
                 String screenNbrFromFirebase = String.valueOf(val);
                 Log.i("LoginFragment", "Screen nbr entered: " + val + " Value from firebase: "+screenNbrFromFirebase);
-                String screenNumber = String.valueOf(Constants.screenNbr);
-                EditText name = (EditText) getActivity().findViewById(R.id.name);
-                Constants.userName = name.getText().toString();
-                //Are we on the right screen
-                Log.i("name",Constants.userName);
-                if (name.getText().toString().equals("")){
-                    Toast.makeText(getActivity(),getResources().getString(R.string.empty_name),Toast.LENGTH_LONG).show(); //Toast message if player hasn't entered name
-                }else{
-                    if (screenNbrFromFirebase.equals(screenNumber)){
-                        Log.i("LoginFragment", "Logged in");
-                        FragmentManager fm;
-                        fm = getFragmentManager();
-                        FragmentTransaction ft = fm.beginTransaction();
-                        ft.replace(R.id.container, new MainFragment());
-                        ft.commit();
-                        Toast.makeText(getActivity(),getResources().getString(R.string.toast_instruction),Toast.LENGTH_LONG).show(); //Toast info message when entering the game
-                    }else{
-                        Toast.makeText(getActivity(),getResources().getString(R.string.toast_incorrect_screen),Toast.LENGTH_LONG).show();
-                    }
-                }
+                Log.i("LoginFragment", "Logged in");
+                FragmentManager fm;
+                fm = getFragmentManager();
+                FragmentTransaction ft = fm.beginTransaction();
+                ft.replace(R.id.container, new MainFragment());
+                ft.commit();
+                Toast.makeText(getActivity(),getResources().getString(R.string.toast_instruction),Toast.LENGTH_LONG).show(); //Toast info message when entering the game
+
             }
         } catch (Exception e) {
             e.printStackTrace();
